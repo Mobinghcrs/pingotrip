12f414c985b2c63accd351ed9ae17a13c8213535
ayoubgharibavi <ayoubgharibavi@users.noreply.github.com>
2025-10-17 16:00:33 +0330

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path for deployment to a subdirectory on a domain.
-  // Replace '/pingo-trip/' with your actual repository name or subdirectory.
-  base: '/pingo-trip/',
+  // Set the base path for deployment to match Liara app name
+  base: '/pingotrip/',
 });
